ba02fed648f4576c1329f1fedbea5b06caef3524
enkasvenka <eriksvedenlund@hotmail.se>
2017-03-06 13:06:00 +0100

    clean

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h3>Femkamp</h3>
-        <h2>eller nåt</h2>
-        <h4>Magico!</h4>
+      <div>
+        redan?
       </div>
     );
   }
